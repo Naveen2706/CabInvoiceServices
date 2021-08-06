cea15fcd47766c2179d674f51f506452c434b775
Naveen2706 <naveen271996@gmail.com>
2021-08-06 22:08:02 +0530

    Cab Invoice Services using Test Driven Development

---
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,80 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoiceGenerator {
+
+    private static final int costPerMinute = 1;
+    private static final double minCostPerKm = 10;
+    private static final double minfare = 5;
+
+    public double calculateFare(double distance, int minute) {
+        double totalfare = (distance * minCostPerKm) + (minute * costPerMinute);
+
+        if (distance < 1.0 && minute <= 5) {
+            return totalfare;
+        } else if (distance < 0.5 && minute <= 2) {
+            return minfare;
+        }
+        return totalfare;
+    }
+
+    public double calculateFare(Ride[] ride) {
+        double totalfare = 0;
+        for (Ride rides : ride) {
+            totalfare += this.calculateFare(rides.distance, rides.minute);
+        }
+        return totalfare;
+    }
+
+    public InvoiceSummary calculateTotalFare(Ride[] ride) {
+        double totalfare = 0;
+        double averageFare = 0;
+        for (Ride rides : ride) {
+            totalfare += this.calculateFare(rides.distance, rides.minute);
+        }
+        averageFare = totalfare / ride.length;
+        return new InvoiceSummary(ride.length, totalfare, averageFare);
+    }
+
+    public double calculateFare(String choice, double distance, int minute) {
+        if (choice.equalsIgnoreCase("Normal")) {
+            int costPerMinute = 1;
+            double minCostPerKm = 10;
+            double minfare = 5;
+            double totalfare = (distance * minCostPerKm) + (minute * costPerMinute);
+            return totalfare;
+        } else if (choice.equalsIgnoreCase("Premium")) {
+            int costPerMinute = 2;
+            double minCostPerKm = 15;
+            double minfare = 20;
+            double totalfare = (distance * minCostPerKm) + (minute * costPerMinute);
+            return totalfare;
+        }
+        double totalfare = (distance * minCostPerKm) + (minute * costPerMinute);
+        return totalfare;
+    }
+
+
+    public InvoiceRecords findInvoice(String userId) throws InvalidInputException {
+        try {
+            List<InvoiceRecords> list = new ArrayList();
+            list.add(new InvoiceRecords("U01", 3, 363, 121));
+            list.add(new InvoiceRecords("U02", 2, 340, 170));
+            list.add(new InvoiceRecords("U03", 4, 208, 52));
+            list.add(new InvoiceRecords("U04", 8, 480, 60));
+
+            InvoiceRecords user = null;
+            for (int i = 0; i < list.size(); i++) {
+                if (userId.equals(list.get(i).getUserId())) {
+                    user = list.get(i);
+                    break;
+                }
+            }
+            return user;
+        } catch (NullPointerException ae) {
+            throw new InvalidInputException("Invalid Input!No Records");
+        }
+    }
+}
---
+++ src/test/java/com/bridgelabz/InvoiceServicesTest.java
@@ -0,0 +1,122 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceServicesTest {
+
+    InvoiceGenerator invoiceGenerator;
+
+    @Before
+    public void setup() throws Exception {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndMinute_shouldReturn_totalFare() {
+        double distance = 20.0;
+        int minute = 15;
+        double fare = invoiceGenerator.calculateFare(distance, minute);
+        assertEquals(215, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndMinute_shouldReturn_minFare() {
+        double distance = 0.3;
+        int minute = 2;
+        double fare = invoiceGenerator.calculateFare(distance, minute);
+        assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndMinute_shouldReturn_totalFare_forMultipleRides() {
+        Ride[] ride = {
+                new Ride(20.0, 15),
+                new Ride(0.3, 2),
+                new Ride(5.0, 3)
+        };
+        double fare = invoiceGenerator.calculateFare(ride);
+        assertEquals(273, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndMinute_shouldReturntotalFare_forMultipleRides() {
+        Ride[] ride = {
+                new Ride(2.0, 15),
+                new Ride(0.5, 1),
+                new Ride(50.0, 30)
+        };
+        double fare = invoiceGenerator.calculateFare(ride);
+        assertEquals(571, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_shouldReturn_invoiceSummary() {
+        Ride[] ride = {
+                new Ride(20.0, 15),
+                new Ride(0.3, 2),
+                new Ride(5.0, 3)
+        };
+        InvoiceSummary invoiceSummary = invoiceGenerator.calculateTotalFare(ride);
+        InvoiceSummary expected = new InvoiceSummary(3, 273, 91);
+        assertEquals(expected, invoiceSummary);
+    }
+
+    @Test
+    public void givenMultipleRides_shouldReturn_InvoiceSummary() {
+        Ride[] ride = {
+                new Ride(2.0, 14),
+                new Ride(0.5, 1),
+                new Ride(50.0, 30)
+        };
+        InvoiceSummary invoiceSummary = invoiceGenerator.calculateTotalFare(ride);
+        InvoiceSummary expected = new InvoiceSummary(3, 570, 190);
+        assertEquals(expected, invoiceSummary);
+    }
+
+    @Test
+    public void givenChoice_shouldReturn_TotalFare() {
+        String choice = "Normal";
+        double distance = 20.0;
+        int minute = 15;
+        double fare = invoiceGenerator.calculateFare(choice, distance, minute);
+        assertEquals(215, fare, 0.0);
+
+    }
+
+
+    @Test
+    public void givenChoice_shouldReturn_TotalFareforGivenChoice() {
+        String choice = "Premium";
+        double distance = 10.0;
+        int minute = 5;
+        double fare = invoiceGenerator.calculateFare(choice, distance, minute);
+        assertEquals(160, fare, 0.0);
+
+    }
+
+    @Test
+    public void givenUserId_shouldReturn_InvoiceSummary() throws InvalidInputException {
+        String user = "U01";
+        InvoiceRecords invoicerecords = invoiceGenerator.findInvoice(user);
+        InvoiceRecords expected = new InvoiceRecords("U01", 3, 363, 121);
+        assertEquals(expected, invoicerecords);
+    }
+
+    @Test
+    public void givenUserId_shouldreturn_InvoiceSummary() throws InvalidInputException {
+        String user = "U04";
+        InvoiceRecords invoicerecords = invoiceGenerator.findInvoice(user);
+        InvoiceRecords expected = new InvoiceRecords("U04", 8, 480, 60);
+        assertEquals(expected, invoicerecords);
+    }
+
+    @Test
+    public void givenUserId_shouldReturn_InvalidInputException() throws InvalidInputException {
+        String user = " ";
+        InvoiceRecords invoicerecords = invoiceGenerator.findInvoice(user);
+        assertEquals(null, invoicerecords);
+    }
+}
